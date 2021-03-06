5c43d34020c90d4b8ce6785d3226c6bcfbb005bd
DESKTOP-3SEQF7A\Razva <razvan_paul.rotaru@stud.acs.upb.ro>
2021-03-06 19:10:08 +0200

    added logs

--- StockChecker.py
+++ StockChecker.py
@@ -1,5 +1,6 @@
 import ssl
 
+import os.path
 import requests
 import time
 import random
@@ -13,12 +14,18 @@ from email.mime.text import MIMEText
 from datetime import datetime
 
 max_price = 3000
+log_file = None
+
 
 def checkAltex():
     status = "Nu au fost gasite produse conform criteriilor selectate."
 
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36', "Upgrade-Insecure-Requests": "1","DNT": "1","Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8","Accept-Language": "en-US,en;q=0.5","Accept-Encoding": "gzip, deflate"}
-    r = requests.get("https://altex.ro/console-ps5/cpl/", headers = headers)
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36',
+        "Upgrade-Insecure-Requests": "1", "DNT": "1",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+        "Accept-Language": "en-US,en;q=0.5", "Accept-Encoding": "gzip, deflate"}
+    r = requests.get("https://altex.ro/console-ps5/cpl/", headers=headers)
     r.encoding = 'utf-8'
     if status in r.text:
         in_stock = False
@@ -26,6 +33,21 @@ def checkAltex():
         in_stock = True
 
     return in_stock
+
+
+h = [
+    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/602.2.14 (KHTML, like Gecko) Version/10.0.1 Safari/602.2.14',
+    'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0',
+]
+
 
 def checkOrange():
     requests.packages.urllib3.disable_warnings()
@@ -47,6 +69,7 @@ def checkOrange():
 
     return in_stock
 
+
 def checkEmag1():
     status = "Stoc epuizat"
 
@@ -63,7 +86,7 @@ def checkEmag1():
 
 
     try:
-        r = requests.get("https://www.emag.ro/consola-playstation-5-digital-edition-so-9396505/pd/DKKW72MBM/", headers=headers)
+        r = requests.get("https://www.emag.ro/consola-playstation-5-digital-edition-so-9396505/pd/DKKW72MBM/")
 
         if status in r.text:
             return False
@@ -72,14 +95,16 @@ def checkEmag1():
         print("Could not check Emag1 stock")
         print(e)
 
+
 def checkEmag2():
     status = "Stoc epuizat"
 
     headers = ({'User-Agent':
                     'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'})
 
+
     try:
-        r = requests.get("https://www.emag.ro/consola-playstation-5-so-9396406/pd/DNKW72MBM/", headers=headers)
+        r = requests.get("https://www.emag.ro/consola-playstation-5-so-9396406/pd/DNKW72MBM/")
 
         if status in r.text:
             return False
@@ -88,8 +113,11 @@ def checkEmag2():
         print("Could not check Emag2 stock")
         print(e)
 
+
 def checkPriceEmag(r):
     try:
+        global log_file
+
         soup = BeautifulSoup(r.content, features="lxml")
         product = soup.find('div', class_='main-product-form')
         if product is None:
@@ -105,15 +133,22 @@ def checkPriceEmag(r):
         price = int(''.join(filter(str.isdigit, p))) / 100
         debug_info = "{:.2f}".format(price)
         if price > max_price:
-            debug_info = "[E STRIGATOR LA CER] " + debug_info + " pentru " + soup.find('h1', class_='page-title').get_text().strip()
+            debug_info = "[E STRIGATOR LA CER] " + debug_info + " pentru " + soup.find('h1',
+                                                                                       class_='page-title').get_text().strip()
         else:
             debug_info += " NICEEEEEEEEEEEEEEEEE"
         print(debug_info)
+
+        now = datetime.now()
+        current_time = now.strftime("%H:%M:%S")
+
+        log_file.write("[{}] {}\n".format(current_time, debug_info))
         return price < max_price
     except Exception as e:
         print("Could not check price")
         print("Error: " + str(e))
         # print("Got: " + str(soup))
+
 
 gmail_user = 'stockcecar@gmail.com'
 to = [
@@ -122,23 +157,23 @@ to = [
     "chris.luntraru@gmail.com"
 ]
 
+
 def sendEmail(server, site, adresa):
     try:
         now = datetime.now()
-
-        if int(str(now.minute)) == 0:
-            server.sendmail(gmail_user, to[0], "Still working")
-
         current_time = now.strftime("%H:%M:%S")
         mesaj = "\n" + "IN STOC: " + site + " LINK: " + adresa + " \n" + "TIMESTAMP: " + current_time
 
         for i in to:
             server.sendmail(gmail_user, i, mesaj)
     except Exception as e:
-        print ('Could not send mail')
+        print('Could not send mail')
         print(e)
 
+
 def start_mail_server():
+    global log_file
+
     gmail_password = 'iskclablyfksortj'
 
     try:
@@ -150,24 +185,36 @@ def start_mail_server():
         server.ehlo()
         server.login(gmail_user, gmail_password)
 
-
         server.sendmail(gmail_user, to[0], "Stock Checker Started")
+
+        now = datetime.now()
+        current_time = now.strftime("%H-%M-%S")
+
+        log_file = open(os.path.join("logs", "log{}.txt".format(current_time)), "w")
+        print(log_file.name)
+
         return server
     except Exception as e:
         print('Could not start server')
         print(e)
 
+
 def stop_mail_server(server):
     try:
+        global log_file
+
         server.close()
+        log_file.close()
         print("server closed")
     except Exception as e:
         print('Could not stop server')
         print(e)
 
+
 def main():
     server = start_mail_server()
     try:
+        index = 0
         while True:
             # if (checkAltex() == True):
             #     sendEmail("Altex", "https://altex.ro/console-ps5/cpl/")
@@ -175,15 +222,20 @@ def main():
             # if (checkOrange() == True):
             #     sendEmail("Orange", "https://www.orange.ro/magazin-online/obiecte-conectate/consola-playstation-5")
             #     print("Orange: Item is in stock")
-            if (checkEmag1() == True):
-                sendEmail(server, "Emag", "https://www.emag.ro/consola-playstation-5-digital-edition-so-9396505/pd/DKKW72MBM/")
+            if checkEmag1():
+                sendEmail(server, "Emag",
+                          "https://www.emag.ro/consola-playstation-5-digital-edition-so-9396505/pd/DKKW72MBM/")
                 print("Emag: Item is in stock")
-            if (checkEmag2() == True):
+            if checkEmag2():
                 sendEmail(server, "Emag", "https://www.emag.ro/consola-playstation-5-so-9396406/pd/DNKW72MBM/")
                 print("Emag: Item is in stock")
             # if (checkGamers() == True):
             #     sendEmail("Gamers", "https://www.gamers.ro/playstation5/playstation-5-825gb")
             #     print("Gamers: Item is in stock")
+            index += 1
+            if index % 100 == 0:
+                server.sendmail(gmail_user, to[0], "Stock Checker did {} checks".format(index))
+
             time.sleep(random.randint(30, 60))
     except KeyboardInterrupt:
         print("stopped")
